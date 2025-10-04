993b894e54b5ef21be9d463be58016557f469c58
MagedGDEV <98350741+MagedGDEV@users.noreply.github.com>
2025-10-04 18:25:36 +0300

    force player in a box

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] private float speed = 5.0f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -16,5 +17,38 @@ public class Player : MonoBehaviour
             0,
             0
         );
+
+        if (transform.position.x > 8.35)
+        {
+            transform.position = new Vector3(
+                8.35f,
+                transform.position.y,
+                0
+            );
+        }
+        else if (transform.position.x < -8.35)
+        {
+            transform.position = new Vector3(
+                -8.35f,
+                transform.position.y,
+                0
+            );
+        }
+        else if (transform.position.y > 4.5)
+        {
+            transform.position = new Vector3(
+                transform.position.x,
+                4.5f,
+                0
+            );
+        }
+        else if (transform.position.y < -4.5)
+        {
+            transform.position = new Vector3(
+                transform.position.x,
+                -4.5f,
+                0
+            );
+        }
     }
 }
