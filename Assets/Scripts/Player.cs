9c030fa9712c8650a3d226cd82aa4c1c913608dc
MagedGDEV <98350741+MagedGDEV@users.noreply.github.com>
2025-10-04 16:52:25 +0300

    Force player to move to the right

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,12 +5,12 @@ public class Player : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Debug.Log("Hello world");
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position += new Vector3(1, 0, 0);
     }
 }
