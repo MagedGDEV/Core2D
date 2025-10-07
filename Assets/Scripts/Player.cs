ca20012cdecebc97d20f1dc2d9ccb61d285e3702
MagedGDEV <98350741+MagedGDEV@users.noreply.github.com>
2025-10-07 12:26:33 +0300

    [C2D-11]: Add player setup with movement and camera follow (#4)

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Player : PhysicsObject
+{
+    [SerializeField] private float speed = 4;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        targetVelocity.x = Input.GetAxis("Horizontal") * speed;
+
+        if (Input.GetButtonDown("Jump") && grounded)
+            velocity.y = 10;
+    }
+}
