87838d21916b617972424e44a9f1a2d7a5074c36
MagedGDEV <98350741+MagedGDEV@users.noreply.github.com>
2025-10-11 19:46:48 +0300

    [C2D-14]: Add HealthBar and Coins Counter to the UI (#7)

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,15 +1,22 @@
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : PhysicsObject
 {
     [SerializeField] private float speed = 4;
     [SerializeField] private float jumpPower = 12;
+    private Vector2 healthBarOrgSize;
+    private int maxHealth = 100;
+    public int health = 50;
     public int coinsCollected = 0;
+    public Text coinsText;
+    public Image healthBar;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        healthBarOrgSize = healthBar.rectTransform.sizeDelta;
+        UpdateUI();
     }
 
     // Update is called once per frame
@@ -19,5 +26,14 @@ public class Player : PhysicsObject
 
         if (Input.GetButtonDown("Jump") && grounded)
             velocity.y = jumpPower;
+    }
+
+    public void UpdateUI()
+    {
+        coinsText.text = coinsCollected.ToString();
+        healthBar.rectTransform.sizeDelta = new Vector2(
+            (float) health / maxHealth * healthBarOrgSize.x,
+            healthBar.rectTransform.sizeDelta.y
+        );
     }
 }
