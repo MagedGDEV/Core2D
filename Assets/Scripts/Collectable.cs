5696a9885313ca1066ad3b283cc76c47d88e3120
MagedGDEV <98350741+MagedGDEV@users.noreply.github.com>
2025-10-11 19:31:02 +0300

    Update health bar when health collectable is triggered

--- Assets/Scripts/Collectable.cs
+++ Assets/Scripts/Collectable.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 public class Collectable : MonoBehaviour
 {
     enum ItemType { Coin, Health, Ammo, Inventory }
-    [SerializeField] ItemType itemType;
+    [SerializeField] private ItemType itemType;
+    private Player player;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        player = GameObject.Find("Player").GetComponent<Player>();
     }
 
     // Update is called once per frame
@@ -22,9 +24,14 @@ public class Collectable : MonoBehaviour
     {
         if (collision.gameObject.name == "Player")
         {
+            if (itemType == ItemType.Coin)
+                player.coinsCollected++;
+            else if (itemType == ItemType.Health)
+            {
+                if (player.health < 100)
+                    player.health+=5;
+            }
 
-            Player player = GameObject.Find("Player").GetComponent<Player>();
-            player.coinsCollected++;
             player.UpdateUI();
             Destroy(gameObject); // gameObject similar to this
         }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,12 +6,17 @@ public class Player : PhysicsObject
 {
     [SerializeField] private float speed = 4;
     [SerializeField] private float jumpPower = 12;
+    private Vector2 healthBarOrgSize;
+    private int maxHealth = 100;
+    public int health = 50;
     public int coinsCollected = 0;
     public Text coinsText;
+    public Image healthBar;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        healthBarOrgSize = healthBar.rectTransform.sizeDelta;
+        UpdateUI();
     }
 
     // Update is called once per frame
@@ -26,5 +31,9 @@ public class Player : PhysicsObject
     public void UpdateUI()
     {
         coinsText.text = coinsCollected.ToString();
+        healthBar.rectTransform.sizeDelta = new Vector2(
+            (float) health / maxHealth * healthBarOrgSize.x,
+            healthBar.rectTransform.sizeDelta.y
+        );
     }
 }
