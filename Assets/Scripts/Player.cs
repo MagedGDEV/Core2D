447228e42da836e1ea437e8419b8e0afed52b1a2
MagedGDEV <98350741+MagedGDEV@users.noreply.github.com>
2025-10-19 22:15:07 +0300

    [C2D-19]: Attacking & Dying (#12)

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,12 +1,16 @@
+using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.AI;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class Player : PhysicsObject
 {
     [SerializeField] private float speed = 4;
     [SerializeField] private float jumpPower = 12;
+    [SerializeField] private float attackDuration = 0.1f;
+    
 
     private Vector2 healthBarOrgSize;
     private int maxHealth = 100;
@@ -14,10 +18,12 @@ public class Player : PhysicsObject
     public Dictionary<string, Sprite> inventory = new Dictionary<string, Sprite>();
     public int health = 50;
     public int coinsCollected = 0;
+    public int attackPower = 25;
     public Text coinsText;
     public Image healthBar;
     public Image inventoryItemImage;
     public Sprite inventoryBlank;
+    [SerializeField] private GameObject attackBox;
 
     private static Player instance;
     public static Player Instance
@@ -42,6 +48,32 @@ public class Player : PhysicsObject
 
         if (Input.GetButtonDown("Jump") && grounded)
             velocity.y = jumpPower;
+
+
+        if (targetVelocity.x > 0.01)
+            transform.localScale = new Vector2(1, 1);
+        else if (targetVelocity.x < -0.01)
+            transform.localScale = new Vector2(-1, 1);
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            StartCoroutine(Attack());
+        }
+
+        if (health <= 0)
+            Die();
+    }
+
+    private IEnumerator Attack()
+    {
+        attackBox.SetActive(true);
+        yield return new WaitForSeconds(attackDuration);
+        attackBox.SetActive(false);
+    }
+
+    private void Die()
+    {
+        SceneManager.LoadScene("SampleScene");
     }
 
     public void UpdateUI()
