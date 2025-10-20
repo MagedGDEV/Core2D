db2830671136ebc7535784f04dc67058328544b0
MagedGDEV <98350741+MagedGDEV@users.noreply.github.com>
2025-10-20 18:13:50 +0300

    script to load new scene

---
+++ Assets/Scripts/SceneLoadTrigger.cs
@@ -0,0 +1,26 @@
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoadTrigger : MonoBehaviour
+{
+    [SerializeField] private string loadSceneName; 
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject == Player.Instance.gameObject)
+        {
+            SceneManager.LoadScene(loadSceneName);
+        }    
+    }
+}
