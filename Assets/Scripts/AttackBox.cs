39fab4235a1d47b449fe7010aa0c04a4ba2372a2
MagedGDEV <98350741+MagedGDEV@users.noreply.github.com>
2026-01-12 19:19:27 +0200

    Add enemy hurt and death sound

--- Assets/Scripts/AttackBox.cs
+++ Assets/Scripts/AttackBox.cs
@@ -2,21 +2,11 @@ using UnityEngine;
 
 public class AttackBox : MonoBehaviour
 {
-    void Start()
-    {
-        
-    }
-
-    void Update()
-    {
-
-    }
-
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.GetComponent<Enemy>())
         {
-            collision.gameObject.GetComponent<Enemy>().health -= Player.Instance.attackPower;
+            collision.gameObject.GetComponent<Enemy>().Hurt();
         }
     }
 }
